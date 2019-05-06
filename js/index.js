f2adc608c025b6f466d0c8e3d900256c268d128f
Dion <dionagoes@gmail.com>
2019-05-06 23:50:13 +0700

    initial commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+// SideNav Button Initialization
+
+$(document).ready(function(){
+    $("#sidebar").mCustomScrollbar({
+        theme: 'minimal'
+    })
+
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active')
+        $('.collapse.in').toggleClass('in');
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false')
+    })
+});
