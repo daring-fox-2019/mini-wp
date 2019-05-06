8f0e6c3da25238759a93dfa1190634ad57bfc96f
Andre Suchitra <89.andre@gmail.com>
2019-05-06 15:17:07 +0700

    home with sidebar

---
+++ client/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+        $('#content').toggleClass('active');
+    });
+    // close dropdowns
+    $('.collapse.in').toggleClass('in');
+    // and also adjust aria-expanded attributes we use for the open/closed arrows
+    // in our CSS
+    $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+});
