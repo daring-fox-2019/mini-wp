8032eeea3b6f78aeb6b9516b836ad89b20152216
ilman <ilman1990@gmail.com>
2019-05-08 11:29:14 +0700

    dummy

---
+++ client/js/main.js
@@ -0,0 +1,49 @@
+Vue.component('Posted', {
+  props: ['article', `index`],
+  data: function () {
+    return {
+      edit: false,
+      showEdit: true,
+    }
+  },
+  template: `
+<div class="card">
+  <div class="card-header">
+    <h5 class="mb-0">
+      <button class="btn btn-link text-dark" type="button" data-toggle="collapse" :data-target="'#post'+index" @click="showEdit = !showEdit">
+        {{article.title}}
+      </button>      
+      <button class="btn btn-info text-dark btn-sm" type="button"  v-if="showEdit" >
+        edit
+      </button>
+      <button class="btn btn-danger text-dark btn-sm" type="button"  v-if="showEdit" >
+        delete
+      </button>
+    </h5>
+  </div>
+  <div :id="'post'+index" class="collapse show">
+    <div class="card-body">
+      {{article.content}}
+    </div>
+  </div>
+</div>`
+})
+
+var app = new Vue({
+  el: '#blog-post',
+  data: {
+    message: 'Hello Vue!',
+    articles: []
+  },
+  methods: {
+    blog_id: function (counter) {
+      return `blog-${counter}`
+    }
+  },
+  mounted() {
+    axios
+      .get('http://localhost:3000/articles')
+      .then(response => (this.articles = response.data))
+      .catch(err => console.log(err))
+  }
+})
---
+++ server/app.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+const express = require('express');
+const app = express();
+const router = express.Router()
+const cors = require('cors')
+const volleyball = require('volleyball')
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+// app.use
+app.use(cors())
+app.use(volleyball)
+app.use('/', (req,res,next)=>{
+    res.send('hallo wp')
+});
+
+
+app.use(function (error, req, res, next) {
+    console.log('~~~catched by last error middlware~~~')
+    if (error.fromRoute) console.log(error.fromRoute)
+    console.log(error)
+    console.log('~~~~~~')
+    if (!error.statusCode) error.statusCode = 500
+    res.status(error.statusCode).json({ message: error.message, error });
+});
+
+app.listen(port, () => console.log(`listening on port ${port}`));
+
