3bdc0915b5315581b5b85c0ab8e6c706b6281880
noviirna <novi.irnawati@gmail.com>
2019-05-11 22:38:55 +0700

    backend tinggal crud

--- server/models/article.js
+++ server/models/article.js
@@ -1,8 +1,18 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
  
+// data : title, snippet, content, createdAt, updatedAt, postedAt, userId, status, image
+
 const ArticleSchema = new Schema({
-  
+  userId : { type: Schema.Types.ObjectId, ref: 'User' },
+  title : String,
+  snippet : String,
+  content : String,
+  createdAt : Date,
+  updatedAt : Date,
+  postedAt : Date,
+  status : String,
+  image : String,
 })
 
 const Articles = mongoose.model('Article', ArticleSchema)
