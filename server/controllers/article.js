fa319cffa37cefbd84102ed9f32fc44bcda8eadb
noviirna <novi.irnawati@gmail.com>
2019-05-12 12:17:26 +0700

    crud done, upload to gcs on going

--- server/controllers/article.js
+++ server/controllers/article.js
@@ -25,21 +25,6 @@ class ControllerArticle {
     })
   }
 
-  // static seeArticles(req,res){
-  //   let condition = {
-  //     userId : new ObjectID(req.headers.id)
-  //   }
-
-  //   Article.find(condition)
-  //   .then(articles=>{
-  //     res.status(201).json(articles)
-  //   })
-  //   .catch(error=>{
-  //     res.status(500).json({message : error.message})
-  //     console.log("error filter article server", error)
-  //   })
-  // }
-
   static seeArticles(req,res){
     let condition = {
       userId : new ObjectID(req.headers.id)
@@ -49,13 +34,14 @@ class ControllerArticle {
       if(articles.length > 0){
         if(req.query.filter){
           let keyword = req.query.filter
-          let result = []
-          articles.forEach((article, i)=>{
-            if((article.title.indexOf(keyword)) == -1){
+          for(let i = 0; i < articles.length; i++){
+            if((articles[i].title.indexOf(keyword)) == -1){
+              console.log(articles[i].title, keyword)
               articles.splice(i, 1)
-            }  
-          })
-          res.status(201).json(result)
+              i--
+            }
+          }
+          res.status(201).json(articles)
         } else {
           console.log("gapake filter query")
           res.status(201).json(articles)
@@ -79,13 +65,45 @@ class ControllerArticle {
       res.json(condition)
     })
     .catch(error=>{
-      res.json({message : error.message})
+      res.status(500).json({message : error.message})
       console.log("error delete article server", error)
     })
   }
 
   static updateArticle(req,res){
-
+    let condition = new ObjectID (req.params.id)
+    let updateValue = {
+      updatedAt : new Date
+    }
+    if(req.body.title && req.body.title != ""){
+     updateValue.title = req.body.title
+    }
+    if(req.body.snippet && req.body.snippet != ""){
+     updateValue.snippet = req.body.snippet
+    }
+    if(req.body.content && req.body.content != ""){
+     updateValue.content = req.body.content
+    }
+    if(req.body.content && req.body.content != ""){
+     updateValue.postedAt = req.body.postedAt
+    }
+    if(req.body.status && req.body.status != ""){
+     updateValue.status = req.body.status
+    }
+    if(req.body.content && req.body.content != ""){
+     updateValue.image = req.body.image
+    }
+    console.log("data yang di update =>", updateValue)
+    
+    Article.findByIdAndUpdate(condition, updateValue)
+    .then(result=>{
+      console.log("hasil update akan ada data yang sebelumnya =>",result)
+      res.status(201).json(result)
+    })
+    .catch(error=>{
+      res.status(500).json({message : error.message})
+      console.log("error update article server", error)
+    })
   }
 }
 
--- server/routes/index.js
+++ server/routes/index.js
@@ -8,16 +8,16 @@ const { sendUploadToGCS, multer } = require('../middlewares/uploadtocloud');
 router.post("/googleLogin", cUser.signInGoogle)
 router.post("/register", cUser.createNewUser) // request data { name, email, password } // response on success {id, name, email, password}
 router.post("/login", cUser.signInUser) // request data : {email, password} // response on success {id, name, email, token}
+
 router.use(authentication)
 router.get("/articles", cArticle.seeArticles) // request headers : token // response on success [{},{}]
-// router.get("/filter", cArticle.filterArticle) // request headers : token // response on success [{},{}]
 
 router.post("/articles", cArticle.newArticle) // request headers : token, id data : title, snippet, content, createdAt, updatedAt, postedAt, userId, status, image
 // response on success {_id, title, snippet, content, createdAt, updatedAt, postedAt, userId, status, image, __v}
-
-router.delete("/articles/:id", authorization, cArticle.deleteArticle) // headers : token, id
+router.put("/articles/:id", authorization, cArticle.updateArticle)
+// response on success {_id, title, snippet, content, createdAt, updatedAt, postedAt, userId, status, image, __v}
+router.delete("/articles/:id", authorization, cArticle.deleteArticle) // headers : token, id 
 // response on success { _id }
-
 
 /**
  * router.post("/uploadimg", multer.single('image'), sendUploadtoGCS,
