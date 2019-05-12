42cc805d02ef2838e876a515439dbd05c55725fe
Fahmisutansyah <fahmisutansyah@gmail.com>
2019-05-13 03:13:11 +0700

    edit app.js for deployment

--- server/app.js
+++ server/app.js
@@ -2,11 +2,12 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const db = process.env.ATLAS_PASSWORD ? `mongodb+srv://admin:${process.env.ATLAS_PASSWORD}@cluster0-ayir7.gcp.mongodb.net/test?retryWrites=true` : `mongodb://localhost/mini-wp`
 const mongoose = require('mongoose')
 const cors = require("cors")
 const morgan = require('morgan')
 
-mongoose.connect(process.env.DB_LOKAL, { 
+mongoose.connect(db, { 
     useNewUrlParser: true,
     useCreateIndex: true
     })
