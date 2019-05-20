09d720382cbe7c2e9dfc3808fb60fd4359463de9
alvinchristian7 <alvin.christian7@yahoo.com>
2019-05-20 16:16:45 +0700

    final

--- client/js/newVue.js
+++ client/js/newVue.js
@@ -1,5 +1,5 @@
 // axios.defaults.baseURL = "http://localhost:3000"
-axios.defaults.baseURL = "http://35.198.248.18"
+axios.defaults.baseURL = "http://mini-wordpress.alvinchristian7.icu"
 // var userName = ""
 Vue.use(CKEditor);
 
