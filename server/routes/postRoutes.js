84d3a400501bb1b3737cecb4c19fcca1376981c6
alvinchristian7 <alvin.christian7@yahoo.com>
2019-05-20 18:25:25 +0700

    tes

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -5,11 +5,11 @@ const Authenticate = require('../middlewares/authenticate')
 const upload  = require('../middlewares/upload')
 const gcs = require('../middlewares/GCS')
 
-router.post('/create', Authenticate, gcs.multer.single("image_url"), gcs.sendUploadToGCS, postCont.create)
+router.post('/create', Authenticate, /* gcs.multer.single("image_url"), */ gcs.sendUploadToGCS, postCont.create)
 router.get('/read', Authenticate, postCont.read)
 router.get('/read/search', Authenticate, postCont.search)
 router.get('/read/:_id', Authenticate, Authorize, postCont.readOne)
-router.put('/update/:_id', Authenticate, Authorize, gcs.multer.single("image_url"), gcs.sendUploadToGCS, postCont.update)
+router.put('/update/:_id', Authenticate, Authorize, /* gcs.multer.single("image_url"), */ gcs.sendUploadToGCS, postCont.update)
 router.delete('/delete/:_id', Authenticate, Authorize, postCont.delete)
 
 module.exports = router
